9dfb99c26396b4cd69069aa8702b2a5e7a2a3d53
unknown <prueba@.matcom.uh.cu>
2013-12-19 10:14:21 -0500

    Little graphic changes for BLOCK PORTS SELECTION (futures in this moment I don't have PyQt for verify it)

--- Visual/BlockVisual.py
+++ Visual/BlockVisual.py
@@ -23,11 +23,21 @@ class QBlock(QGraphicsItem):
         self.height = QBlock.DX*(max(len(self.block.input_ports), len(self.block.output_ports))+1)
         # self.width = self.height/
         self.width = 40
+        self.inEllipses = [] # in ports endings
+        self.outEllipses = []# out ports endings
 
         self.rect = QRectF(-QBlock.PORT_SIZE,0,2*QBlock.PORT_SIZE + self.width,self.height).adjusted(0.5,0.5,0.5,0.5)
         # self.rectf = QRectF(-QBlock.PORT_SIZE,0,QBlock.PORT_SIZE+self.width,self.height).adjusted(0.1,0.1,0.1,0.1)
 
         self.setFlag(QGraphicsItem.ItemIsMovable)
+
+        # for i in range(len(self.block.input_ports)):
+        #     self.inEllipses.append(QGraphicsEllipseItem(-QBlock.PORT_SIZE,di*(i + 1),0,di*(i + 1), 3)
+        #     self.inEllipses[-1].setCursor(Qt.WhatsThisCursor)
+        #
+        # for i in range(len(self.block.output_ports)):
+        #     self.outEllipses.append(QGraphicsEllipseItem(self.width+ QBlock.PORT_SIZE,do*(i + 1)), 3)
+        #     self.outEllipses[-1].setCursor(Qt.WhatsThisCursor)
 
     def boundingRect(self):
         return self.rect
@@ -47,7 +57,9 @@ class QBlock(QGraphicsItem):
         # Drawing input ports
         for i in range(len(self.block.input_ports)):
             painter.drawLine(-QBlock.PORT_SIZE,di*(i + 1),0,di*(i + 1))
+            # painter.drawEllipse(self.inEllipses[i])
 
         # Drawing output ports
         for i in range(len(self.block.output_ports)):
             painter.drawLine(self.width ,do*(i + 1),self.width+ QBlock.PORT_SIZE,do*(i + 1))
+            # painter.drawEllipse(self.outEllipses[i])
